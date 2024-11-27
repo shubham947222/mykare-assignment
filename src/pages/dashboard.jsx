35d474ccd7cc206e6920fc6fb8ec572b436291e4
shubham947222 <shubham947222@gmail.com>
2024-11-27 10:34:16 +0530

    minor comma dot fixes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/contexts/AuthContext";
 import withAuth from "@/hoc/authRoute";
-import UserDashboard from "@/components/charts/UserDashboard";
 const Dashboard = () => {
   const { user, logout } = useAuth();
   const router = useRouter();
@@ -17,7 +16,7 @@ const Dashboard = () => {
       <div className="bg-white p-8 rounded-xl shadow-xl w-full sm:w-96 flex flex-col items-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome, {user}!</h1>
         <p className="text-gray-600 mb-6 text-center">
-          You're now logged in. Explore your dashboard or log out.
+          You are now logged in Explore your dashboad
         </p>
         <button
           className="bg-red-500 text-white py-2 px-6 rounded-lg shadow-md hover:bg-red-600 transition duration-300"
